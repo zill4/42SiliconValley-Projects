0db8248960c97376a80cc2d80099463d6be403d3
zill4 <justcrisp@outlook.com>
2019-01-15 08:57:31 -0800

    gen map dimensions

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -5,4 +5,16 @@
 # include <fcntl.h>
 # include <stdio.h>
 
+typedef struct s_point
+{
+    int x; //The position on the horizontal axis.
+    int y; //The position on the vertical axis.
+    int z; //The value of the point axis.
+}               t_point;
+typedef struct s_map
+{
+    t_point **map;
+    int dimensions; //the overall dimensions of the map.
+}               t_map;
+
 #endif
--- fdf/src/main.c
+++ fdf/src/main.c
@@ -1,11 +1,54 @@
 #include "../includes/fdf.h"
+
+void    draw_grid(void* mlx_ptr, void *win_ptr, int grid)
+{
+    grid++;
+    for (int k = 1; k < 12; k++)
+    {
+        for (int i = 0; i < 300; i++)
+        {
+            mlx_pixel_put(mlx_ptr, win_ptr, i + 100 , 400 - (26 * k), 255);
+            mlx_pixel_put(mlx_ptr, win_ptr, 400 - (26 * k) ,i + 100 , 255);
+        }
+    }
+}
+t_point getDimensions(int fd)
+{
+    t_point dim;
+    char *line;
+    int x;
+    int i;
+    int y;
+    i = 0;
+    y = 0;
+    x = 0;
+    while (get_next_line(fd, &line))
+    {
+        /* processes map into numeric map */
+        ft_putstr(line);
+        while(line[i])
+        {
+            //Check if digit is surrounded by ' 's such that it constitutes a value.
+            if (ft_isdigit(line[i]) && line[i -1] == ' ' && line[i + 1] == ' ')
+                x++;
+            i++;
+        }
+        dim.x = x;
+        ft_putchar('\n');
+        y++;
+    }
+    dim.x += 2;
+    dim.y = y;
+      printf("\nDimensions X: %d Y: %d \n", dim.x, dim.y);
+    return (dim);
+}
+
 int main(int argc, char **argv)
 {
     /* VARIABLES */
     // File Descriptor
     int fd;
     // temporary string container
-    char *line;
     printf("Welcome to FdF!\n");
 
     /* We are first going to read in a map file */
@@ -14,17 +57,23 @@ int main(int argc, char **argv)
         ft_putstr("Usage: FdF input file\n");
         return (1);
     }
+    // Open file for initial dimension check.
     fd = open(argv[1], O_RDONLY);
-    while (get_next_line(fd, &line))
-    {
-        ft_putstr(line);
-        ft_putchar('\n');
-    }
-	void *mlx_ptr;
+    // Get dimensions
+    t_point dim = getDimensions(fd);
+    // print dimensions
+      printf("\nDimensions X: %d Y: %d \n", dim.x, dim.y);
+    /* testing inputs */
+    void *mlx_ptr;
 	void *win_ptr;
-
+    int grid = 12;
+    t_point one;
+    one.x = 10;
+    one.y = 10;
+    one.z = 42;
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "FdF");
+    draw_grid(mlx_ptr, win_ptr, grid);
 	mlx_loop(mlx_ptr);
     return (0);
 }
